2d5622520f76a63ec151b023897ab3e1ee56abc1
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2012-10-29 18:07:30 +0000

    alpha Contact.sms api

--- api/v3/Contact/Sms.php
+++ api/v3/Contact/Sms.php
@@ -16,22 +16,44 @@
  */
 
 function civicrm_api3_contact_sms($params) {
-	if(!$id = CRM_Utils_Array::value('id', $params)){
-		return civicrm_api3_create_error('You need to supply an ID to SMS a contact');
+		
+	//This API makes everything nice for / wraps around
+	//CRM_Activity_BAO_Activity::sendSMS()
+	
+	//it would be nice to be able to chain this by sending the results of a Contact.get
+	
+
+	//get the list of contacts that you want to send the SMS to
+	if(!isset($params['id'])){
+		return civicrm_api3_create_error('Please include a contact id.');
 	}
-	require_once('CRM/Core/Error.php');
-
-	require_once('CRM/Activity/BAO/Activity.php');
-	$tokenText = CRM_Utils_Token::replaceContactTokens('Test', $values, FALSE, $messageToken, FALSE, $escapeSmarty);
-	$tokenText = CRM_Utils_Token::replaceHookTokens($tokenText, $values, $categories, FALSE, $escapeSmarty);
-	echo $tokenText;
-
-	// for some reason, we need to add some smsParams here - not sure where to get them from ATM
-	$smsParams=array();
-
-	CRM_Activity_BAO_Activity::sendSMSMessage($id, $tokenText, $tokenText, $smsParams, 1);
-	CRM_Core_Error::debug_log_message('Sending SMS...');
-
+	$contactsResult = civicrm_api('Contact', 'get', array('version'=>3, 'id' => $params['id']));
+	//$contactsResult = civicrm_api('Contact', 'get', $params);
+	
+	if(!$contactsResult['count']){
+		return civicrm_api3_create_error('Please specify at least one contact.');
+	}
+	$contactDetails = $contactsResult['values'];
+	//idea is that this contact will take a contact ID and a text message and then send an SMS
+	
+	foreach($contactDetails as $contact){
+		$contactIds[]=$contact['contact_id'];
+	}
+	
+	// use the default SMS provider
+	$providers=CRM_SMS_BAO_Provider::getProviders(NULL, array('is_default' => 1));
+	$provider = $providers[0];
+	$provider['provider_id'] = $provider['id'];
+	
+	//this should be set somehow when not set (or maybe we need to change the underlying BAO to not require it?)
+	$userID=1;
+	
+	$activityParams['text_message']=$params['text'];
+	
+	CRM_Activity_BAO_Activity::sendSMS($contactDetails, $activityParams, $provider, $contactIds, $userID);
+	
 	return civicrm_api3_create_success();
-	return civicrm_api3_create_error('Could not SMS contact');
+		
+
+
 }
